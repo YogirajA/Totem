5c54272e6c8512d379207008d7b2443db66e266d
Saagar Minocha <saagar.minocha@headspring.com>
2019-09-26 17:09:54 -0500

    Added a testexample target for the build command to run the totem app, seed the db with the example data and then run the Sales Order example app tests against it. The Totem app in the background is then killed. Added seed-database-for-examples function in build-helpers to seed the Dev db with an example contract.

--- build.ps1
+++ build.ps1
@@ -7,6 +7,7 @@ help {
     write-help "build rebuild" "Builds a clean local copy, rebuilding databases instead of updating them."
     write-help "build ci 1234" "Continuous integration build, applying build counter to assembly versions."
     write-help "build migratetest" "Updates test database with test environment scripts instead of rebuilding"
+	write-help "build testexample" "Tests an example app with Totem"
 }
 
 function publish($project) {
@@ -16,7 +17,7 @@ function publish($project) {
 }
 
 main {
-    validate-target "default" "rebuild" "ci" "migratetest"
+    validate-target "default" "rebuild" "ci" "migratetest" "testexample"
 
     $targetFramework = "netcoreapp2.1"
     $configuration = 'Release'
@@ -48,6 +49,17 @@ main {
     }
 
     task "Test" { dotnet test --configuration $configuration --no-build } src/Totem.Tests
+	
+	if ($target -eq "testexample") {
+	  write-host "Running Totem" -ForegroundColor Cyan
+	  $procId = (start 'dotnet' -verb runas -argumentlist "run -p src/Totem/Totem.csproj" -passthru).ID
+	  task "Seed DB with example data" { seed-database-for-examples DEV } src/Totem.DatabaseMigration
+	  try {
+		task "Run Sales Order example app" {dotnet test examples/SalesOrderApp/SalesOrderApp.csproj}
+	  } finally {
+		task "Closing Totem" { start 'taskkill' -verb runas -argumentlist "/F /PID $procId /T" }
+	  }  	  
+    }
 
     if ($target -eq "ci") {
         delete-directory $publish
