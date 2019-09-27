8e8418f3636201159207c5a47b4cab34d895c9c8
Saagar Minocha <saagar.minocha@headspring.com>
2019-09-27 13:07:40 -0500

    Removed "ValidOrderCompletedContractId" from the appsettings and Totem Settings as it is not being used currently.

--- examples/SalesOrderApp/TotemSettings.cs
+++ examples/SalesOrderApp/TotemSettings.cs
@@ -4,6 +4,5 @@
     {
         public string TestMessageApiUrl { get; set; }
         public string ValidPlaceOrderContractId { get; set; }
-        public string ValidOrderCompletedContractId { get; set; }
     }
 }
