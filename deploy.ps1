da48f7c0184cd0774b80cdc4b6a8ede87c090535
YogirajA <aradhyeyogi@gmail.com>
2020-07-24 10:17:14 -0500

    Cleaned out deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -3,11 +3,7 @@ param($target="default", [int]$buildNumber)
 . .\build-helpers
 
 help {
-    write-help "build (default)" "Optimized for local development: updates databases instead of rebuilding them."
-    write-help "build rebuild" "Builds a clean local copy, rebuilding databases instead of updating them."
-    write-help "build ci 1234" "Continuous integration build, applying build counter to assembly versions."
-    write-help "build migratetest" "Updates test database with test environment scripts instead of rebuilding"
-    write-help "build testexample" "Tests an example app with Totem"
+    write-help "deploy" "updates databases instead of rebuilding them."  
 }
 
 function publish($project) {
@@ -17,18 +13,17 @@ function publish($project) {
 }
 
 main {
-    validate-target "default" "rebuild" "ci" "migratetest" "testexample"
+    validate-target "default"
 
     $targetFramework = "netcoreapp3.1"
     $configuration = 'Release'
     $product = "Totem"
-    $yearInitiated = 2020
+    $yearInitiated = (Get-Date).Year
     $owner = "Headspring"
     $publish = "$(resolve-path .)/publish"
 
     $connectionStrings = @{
         DEV = connection-string EmployeeDirectory src/Totem/appsettings.json;
-        TEST = connection-string EmployeeDirectory src/Totem.Tests/appsettings.json
     }
 
     task ".NET Environment" { dotnet --info }
@@ -44,39 +39,8 @@ main {
     }
     task "Switch directlry back to root" {Set-Location ..\.. }
     task "Build" { dotnet build --configuration $configuration --no-restore /nologo } src
-    task  "SQLLocalDB" {SqlLocalDB.exe start}
-
-    if ($target -eq "default") {
-        task "Update DEV/TEST Databases" { update-database DEV } src/Totem.DatabaseMigration
-    }
-    elseif ($target -eq "rebuild") {
-        task "Rebuild DEV/TEST Databases" { rebuild-database DEV } src/Totem.DatabaseMigration
-    }
-    elseif ($target -eq "ci") {
-        task "Rebuild TEST Database" { rebuild-database } src/Totem.DatabaseMigration
-    }
-    elseif ($target -eq "migratetest") {
-        task "Update TEST Database" { update-database } src/Totem.DatabaseMigration
-    }
-
-    <# task "Test" { dotnet test --configuration $configuration --no-build } src/Totem.Tests
-	
-    if ($target -eq "testexample") {
-        task "Seed DB with example data" { seed-database-for-examples DEV } src/Totem.DatabaseMigration
-        write-host "Running Totem" -ForegroundColor Cyan
-        $procId = (start 'dotnet' -verb runas -argumentlist "run -p src/Totem/Totem.csproj" -passthru).ID
-        try {
-            task "Run Sales Order example app" { dotnet test examples/SalesOrderApp/SalesOrderApp.csproj }
-        }
-        finally {
-            task "Closing Totem" { start 'taskkill' -verb runas -argumentlist "/F /PID $procId /T" }
-        } 
-    }
-    
-    task "Removing variables" { gci Env: | where Name -clike 'NPM_CONFIG_*' | remove-item}
-
-    task "Javascript Test" { npm run test --prefix src/Totem/ }
-    #>
+ 
+    task "Update DEV/TEST Databases" { update-database DEV } src/Totem.DatabaseMigration
 
     delete-directory $publish
     publish Totem
